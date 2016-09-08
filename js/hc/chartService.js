6d10f37820e42dbe469087a8e5102e4f5d3a82ce
himasnhusharma <himanshu.sharma@oodlestechnologies.com>
2016-09-08 18:53:26 +0530

    calc

--- js/hc/chartService.js
+++ js/hc/chartService.js
@@ -1,5 +1,5 @@
 app.service('ChartServiceHc',function(){
-  this.createChart = function(containerCC,title,thpWithoutSS,optimisedSS,changeTheme){
+  this.createChart = function(containerCC,title,Now,Required,changeTheme){
 
     var HCDefaults = $.extend(true, {}, Highcharts.getOptions(), {});
     
